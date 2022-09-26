c6a5769906968da92dd8c902dd0f807ecef4d3b5
nekago <nekarulez@gmail.com>
2022-09-26 15:30:10 +0300

    refactoring structure

--- src/app/modules/character-detail/character-detail.module.ts
+++ src/app/modules/character-detail/character-detail.module.ts
@@ -3,18 +3,10 @@ import {CommonModule} from "@angular/common";
 import {LoaderModule} from "../../global/modules/loader/loader.module";
 import {CharacterDetailComponent} from "./components/character-detail.component";
 import {HeaderModule} from "../header/header.module";
+import {ControlSectionModule} from "../control-section/control-section.module";
 
 @NgModule({
-  declarations: [
-    CharacterDetailComponent
-  ],
-    imports: [
-        CommonModule,
-        LoaderModule,
-        HeaderModule
-    ]
+  declarations: [CharacterDetailComponent],
+  imports: [CommonModule, LoaderModule, HeaderModule, ControlSectionModule],
 })
-
-export class CharacterDetailModule {
-
-}
+export class CharacterDetailModule {}
--- src/app/modules/header/header.module.ts
+++ src/app/modules/header/header.module.ts
@@ -1,19 +1,11 @@
-import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
-import {HeaderComponent} from "./components/header.component";
-import {HeaderLogoComponent} from "./components/header-logo/header-logo.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { HeaderLogoComponent } from './components/header-logo/header-logo.component';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    HeaderLogoComponent
-  ],
-  exports: [
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent, HeaderLogoComponent],
+  exports: [HeaderComponent],
+  imports: [CommonModule],
 })
-
 export class HeaderModule {}
