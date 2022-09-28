a3eb948b9a1f455d8f10dbe99b173a024f12e223
nekago <nekarulez@gmail.com>
2022-09-28 15:08:26 +0300

    buttons upgrade and refactor character-list.component

--- src/app/modules/character-list/components/character-list/character-list.component.ts
+++ src/app/modules/character-list/components/character-list/character-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subject, takeUntil, tap } from 'rxjs';
+import { lastValueFrom, Observable, Subject, takeUntil } from 'rxjs';
 import { CharactersListData } from '../../../../global/entities/character-list-data.interface';
 import { CharacterListService } from '../../services/character-list.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-character-list',
@@ -13,33 +13,46 @@ export class CharacterListComponent implements OnInit, OnDestroy {
   currentPage!: number;
   countCards!: number;
   isLoading: boolean = false;
+  public characterListData: CharactersListData | null = null;
+  public characterListError: any | null = null;
 
   characters$: Observable<CharactersListData> =
     new Observable<CharactersListData>();
 
   constructor(
     private characterListService: CharacterListService,
-    private readonly route: ActivatedRoute
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
     this.route.queryParams
       .pipe(takeUntil(this.ngDestroy$))
-      .subscribe((params) => {
+      .subscribe(async (params) => {
         this.currentPage = +params['page'] || 1;
         this.isLoading = true;
-        this.characters$ = this.characterListService
-          .getCharacterList(params)
-          .pipe(tap(() => (this.isLoading = false)));
-        this.characters$.pipe(takeUntil(this.ngDestroy$)).subscribe((data) => {
-          this.countCards = data.info.count;
-          this.characterListService.setLastCharacterId = this.countCards;
-        });
+        // @ts-ignore
+        await this.getCharacterList(params);
       });
   }
 
   ngOnDestroy() {
     this.ngDestroy$.next();
     this.ngDestroy$.complete();
+  }
+
+  public async getCharacterList(params: Params) {
+    try {
+      this.characterListData = await lastValueFrom(
+        this.characterListService.getCharacterList(params)
+      );
+      this.characterListError = null;
+      this.characterListService.setLastCharacterId =
+        this.characterListData.info.count;
+    } catch (e) {
+      this.characterListData = null;
+      this.characterListError = e;
+    } finally {
+      this.isLoading = false;
+    }
   }
 }
